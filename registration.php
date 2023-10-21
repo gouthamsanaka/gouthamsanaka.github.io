e8b18819dee0dae581d8fe1ba3fa33b5d013eeaf
gouthamsanaka <145680926+gouthamsanaka@users.noreply.github.com>
2023-10-22 00:04:26 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,30 +1,19 @@
 <?php
-require 'db_connect.php';    
+include 'db_connect.php'; 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $userName = $_POST['userName'];
-    $password = $_POST['password'];
-    $emailId = $_POST['emailId'];
+    $userName = $_POST["userName"];
+    $emailId = $_POST["emailId"];
 
-    // Hash the password using password_hash
-    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+    // Insert data into the database
+    $sql = "INSERT INTO users (userName, emailId) VALUES ('$userName', '$emailId')";
 
-    // SQL query to insert user data into the database
-    $sql = "INSERT INTO users_details (firstName, lastName, userName, password, emailId) VALUES (?, ?, ?, ?, ?)";
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param('sssss', $firstName, $lastName, $userName, $hashedPassword, $emailId);
-
-
-
-    
-
-    if ($stmt->execute()) {
-        echo "Registration successful.";
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully";
     } else {
-        echo "Error: " . $stmt->error;
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
-    $stmt->close();
 }
+
+$conn->close();
 ?>
